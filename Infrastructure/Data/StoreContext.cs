94efba9b21f3fe2f415d7485e18565b141895744
TanNHN <nhattan9101999gmail.com>
2025-09-28 20:58:53 +0700

    Init project

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+using Core.Entities;
+using Infrastructure.Config;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class StoreContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfig).Assembly);
+    }
+}
